3371640c17b0b8412260d793b401c0f13e794661
Onur Salk <salk.onur@gmail.com>
2016-12-08 13:20:46 +0300

    edited send_email

--- send_email.py
+++ send_email.py
@@ -6,7 +6,6 @@ key_name='mymood.png'
 myrandom=str(random.random())
 
 data='<html><body><img src="https://s3-eu-west-1.amazonaws.com/' + bucket + '/' + key_name + '?a=' + myrandom + '" /></body></html>'
-data1='<a href="spotify:user:spotify:playlist:65V6djkcVRyOStLd8nza8E">Play now</a>'
 client = boto3.client('ses')
 
 def send_email():
